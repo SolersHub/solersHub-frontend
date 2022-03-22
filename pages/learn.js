dabd1d25b128218a8c008865331bab867bd4f23c
emexx2002 <aguchukwuemekag@gmail.com>
2022-03-22 19:47:58 +0100

    learn

--- pages/learn.js
+++ pages/learn.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Header from "components/Header/Header.js";
 import HeaderLinks from "components/Header/HeaderLinks.js";
 import Link from "next/link";
