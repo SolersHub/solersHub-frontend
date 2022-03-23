801bd650f33bc0e36bc5febb69ac7f0147e6ddb5
emexx2002 <aguchukwuemekag@gmail.com>
2022-03-23 23:42:36 +0100

    n

--- pages/admin/courses/[id]/manage.js
+++ pages/admin/courses/[id]/manage.js
@@ -118,7 +118,7 @@ export default function searchquery(props) {
                                         <GridContainer>
                                             <GridItem md={6} >
                                                 <div className={classes.containerFluid}>
-                                                    <img src={item.image ? `https://solershub-backend.herokuapp.com/${item.image}` : "/img/placeholder2.jpg"} style={{ width: "100%", height: "auto" }} />
+                                                    <img src={data.image ? `https://solershub-backend.herokuapp.com/${data.image}` : "/img/placeholder2.jpg"} style={{ width: "100%", height: "auto" }} />
 
                                                 </div>
 
